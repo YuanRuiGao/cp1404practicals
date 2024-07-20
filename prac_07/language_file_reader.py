5e0e6e83e661ce75859b55b14a67e03746d3ab1e
YuanRuiGao <yuanrui.gao@my.jcu.edu.au>
2024-07-20 10:22:51 -0400

    finish language file

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -6,7 +6,6 @@ File and class example - opens/reads a file, stores in objects of custom class
 
 import csv
 from collections import namedtuple
-
 from programming_language import ProgrammingLanguage
 
 
@@ -28,7 +27,7 @@ def main():
         reflection = parts[2] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), parts[4])
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
@@ -39,7 +38,7 @@ def main():
         print(language)
 
 
-main()
+# main()
 
 
 def using_csv():
@@ -63,7 +62,7 @@ def using_namedtuple():
     file_field_names = in_file.readline().strip().split(',')
     print(file_field_names)
     # Language will be a new subclass of the tuple data type class
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, pointer')
     reader = csv.reader(in_file)  # use default dialect, Excel
 
     for row in reader:
@@ -78,11 +77,12 @@ def using_namedtuple():
 
 def using_csv_namedtuple():
     """Language file reader version using both csv module and named tuple."""
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, pointer')
     in_file = open("languages.csv", "r")
     in_file.readline()
     for language in map(Language._make, csv.reader(in_file)):
         print(language.name, 'was released in', language.year)
         print(repr(language))
 
-# using_csv_namedtuple()
+
+using_csv_namedtuple()
--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -2,41 +2,63 @@
 CP1404/CP5632 Practical - Suggested Solution
 Programming Language class with tests.
 """
+import csv
 
 
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, pointer):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer = pointer
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        language_pointer = "no"
+        if self.pointer:
+            language_pointer = ""
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year},"
+                f"language with {language_pointer} pointer")
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
 
+    def is_pointer(self):
+        return self.pointer == "Yes"
 
-def run_tests():
-    """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-    languages = [ruby, python, visual_basic]
-    print(python)
+def main():
+    languages = []
+    in_file = open('languages.csv', 'r')
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split(',')
+        reflection = parts[2] == "Yes"
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), parts[4])
+        languages.append(language)
+    in_file.close()
+    run_tests(languages)
 
-    print("The dynamically typed languages are:")
+
+def run_tests(languages):
+    """Determine whether there is pointer"""
+    # ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, True)
+    # python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    # visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
+    #
+    # languages = [ruby, python, visual_basic]
+    # print(python)
+
+    print("The programming languages with pointer are:")
     for language in languages:
-        if language.is_dynamic():
+        if language.is_pointer():
             print(language.name)
 
 
 if __name__ == "__main__":
-    run_tests()
+    main()
