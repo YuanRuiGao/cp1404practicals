1d80249595a5057e89d6f7e21f5286046a744be3
YuanRuiGao <yuanrui.gao@my.jcu.edu.au>
2024-08-04 03:49:54 -0400

    prac 9 Work

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,6 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+silver_service_taxi1 = SilverServiceTaxi("Hummer", 200, 4)
+print(silver_service_taxi1)
+silver_service_taxi1.drive(18)
+print(silver_service_taxi1.get_fare())
---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,5 @@
+from prac_09.unreliable_car import UnreliableCar
+
+my_unreliable_car = UnreliableCar("BMW", 100, 50)
+# my_unreliable_car.drive(100)
+print(my_unreliable_car.drive(125))
